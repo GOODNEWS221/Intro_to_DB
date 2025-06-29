ea661ae902f7ed678908a78b6ce971ee96f6785f
Goodnews Atekha Osas <154696005+GOODNEWS221@users.noreply.github.com>
2025-06-29 20:23:52 +0100

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -18,7 +18,7 @@ CREATE TABLE Books (
 );
 
 
-CREATE TABLE Customers (
+CREATE TABLE customer_name (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
